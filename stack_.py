0427b0bea5ea1df20be5c9cf5a36c7b8912643b0
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-09 16:52:03 +0500

    update

--- stack_.py
+++ stack_.py
@@ -28,11 +28,12 @@ print("isEmpty: ", isEmpty)
 #Size 
 print("Size: ", len(stack))
 
-# Creating using stack
+# Creating stack using class
 
 class Stack:
     def __init__(self):
         self.stack=[]
+
     def push(self,element):
         return self.stack.append(element)
     
@@ -68,3 +69,61 @@ print(f"Stack after the pop   {mystack.stack}")
 print(f"Peek value of stack:   {mystack.peek()}")
 print(f"MY Stck isEmpty  {mystack.isEmpty()}")
 print(f"Size {mystack.size()}")
+
+
+
+
+# Stack implementation using Linked List 
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
+        self.size +=1
+    
+    def pop(self):
+        if self.isEmpty():
+            return "stack is Empty"
+        popped_node = self.head
+        self.head = self.head.next
+        self.size  -=1
+        return popped_node.value
+    
+    def peek(self):
+        if self.isEmpty():
+            return "stack is Empty"
+        return self.head.value
+    
+    def isEmpty(self):
+        return self.size ==0
+    
+    def stackSize(self):
+        return self.size
+    
+
+
+
+# create a stack ot stack object
+print("\n Calling the Class Object")
+myStack = Stack()
+myStack.push('A')
+myStack.push('B')
+myStack.push('D')
+myStack.push(7)
+# print("Stack", myStack)
+print(f"Pop  {myStack.pop()}")
+print(f"Stack after the pop   {myStack.pop}")
+print(f"Peek value of stack:   {myStack.peek()}")
+print(f"MY Stck isEmpty  {myStack.isEmpty()}")
+print(f"Size {myStack.stackSize()}")
