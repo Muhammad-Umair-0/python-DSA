53cd82e16e2a2ce717b8f244d6daa908b6cecffb
Muhammad-Umair-0 <Malikmu1321@gmail.com>
2025-07-09 14:57:24 +0500

    updated

--- linkedList.py
+++ linkedList.py
@@ -180,3 +180,146 @@ print("\n Traversed Linked List ")
 traversePrint(node1)
 
 
+#  List to find the lowest number 
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def findLowest(head):
+    minVal = head.data
+    currentNode = head.next
+    while currentNode:
+        if currentNode.data < minVal:
+            minVal = currentNode.data
+
+        currentNode = currentNode.next
+    return minVal
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(12)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+print("The Lowest number is ")
+print(findLowest(node1))
+
+
+# dlete a node in linked list
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+    
+def traverseAndPrint(head):
+    currentNode = head
+    while currentNode:
+        print(currentNode.data, end="-->")
+        currentNode  = currentNode.next
+    print("Null")
+
+def delSpecificNode(head, nodeToDelete):
+
+    if head == nodeToDelete:
+        return head.next
+    
+    currentNode = head
+    while currentNode.next and currentNode.next != nodeToDelete :
+        currentNode = currentNode.next
+
+
+    if currentNode.next is None:
+        return head
+    
+    currentNode.next = currentNode.next.next
+
+    return head
+
+
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+
+print("Before Deletion : ")
+traverseAndPrint(node1)
+
+
+
+# deletion of nod
+node1 = delSpecificNode(node1,node4)
+print("\n after deletion ")
+traverseAndPrint(node1)
+
+
+# inseriton of Node
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+    
+def traverseAndPrint(head):
+    currentNode = head
+    while currentNode:
+        print(currentNode.data, end="-->")
+        currentNode  = currentNode.next
+    print("Null")
+
+
+def insertNodeatPosition(head, newNode, position):
+    if position ==1:
+        newNode.next = head
+        return newNode
+    
+    currentNode = head
+    for _ in range(position-2):
+        if currentNode is None:
+            break
+        currentNode = currentNode.next
+
+    newNode.next = currentNode.next
+    currentNode.next = newNode
+    return head
+
+
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+
+print(" Origional list: ")
+traverseAndPrint(node1)
+
+
+
+# deletion of nod
+newNode = Node(97)
+node1 = insertNodeatPosition(node1,newNode,4)
+print("\n after insertion ")
+traverseAndPrint(node1)
--- test.py
+++ test.py
@@ -1,4 +1,52 @@
-n=10
-for i in range(1, n):
-    for j in range(i-1,-1,-1):
-        print()
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+    
+def traverseAndPrint(head):
+    currentNode = head
+    while currentNode:
+        print(currentNode.data, end="-->")
+        currentNode  = currentNode.next
+    print("Null")
+
+
+def insertNodeatPosition(head, newNode, position):
+    if position ==1:
+        newNode.next = head
+        return newNode
+    
+    currentNode = head
+    for _ in range(position-2):
+        if currentNode is None:
+            break
+        currentNode = currentNode.next
+
+    newNode.next = currentNode.next
+    currentNode.next = newNode
+    return head
+
+
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+
+print(" Origional list: ")
+traverseAndPrint(node1)
+
+
+
+# deletion of nod
+newNode = Node(97)
+node1 = insertNodeatPosition(node1,newNode,4)
+print("\n after insertion ")
+traverseAndPrint(node1)
